5c37f1e49a9704176741c9f0c60c22d344c2c12e
Niccolò Burrini <128602579+NiccoloBurrini@users.noreply.github.com>
2023-12-19 22:58:19 +0100

    Delete target/classes directory

--- target/classes/js/index.js
+++
@@ -1,6 +0,0 @@
-function show() {
-            document.getElementById('image')
-                .style.display = "block";
-            document.getElementById('btnID')
-                .style.display = "none";
-        }
